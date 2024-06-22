1705a1226321582ee3385c225b8b36d290b5f77c
Lev Zharikov <33627925+leozaur1808@users.noreply.github.com>
2024-06-22 22:25:16 +0300

    Remove Nvim from config

--- install.sh
+++ install.sh
@@ -45,7 +45,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     fi
 
     # List of packages to install
-    BREW_PACKAGES=("git" "neovim" "node")
+    BREW_PACKAGES=("git" "node")
 
     # Install each package if not already installed
     for package in "${BREW_PACKAGES[@]}"; do
@@ -61,7 +61,7 @@ elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
     sudo apt update
 
     # List of packages to install
-    APT_PACKAGES=("git" "curl" "neovim" "node")
+    APT_PACKAGES=("git" "curl" "node")
 
     # Install each package if not already installed
     for package in "${APT_PACKAGES[@]}"; do
